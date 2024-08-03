22d67f53592ab262a92404e26dfe12b7f11cf54e
saurabh-anthwal <saurabhanthwal@gmail.com>
2024-08-03 19:28:04 +0530

    urls changes

--- useragent/api/urls.py
+++ useragent/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from api.views import AgentView, AgentRunCreateView
 
 urlpatterns = [
-    path('agents', AgentView.as_view(), name='agents'),
+    path('agents/', AgentView.as_view(), name='agents'),
     path('agents/<uuid:agent_id>/', AgentView.as_view(), name='agent-detail'),
     path('agent/run/', AgentRunCreateView.as_view(), name='agent-run-create'),
 
